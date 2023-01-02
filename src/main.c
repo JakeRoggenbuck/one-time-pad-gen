76b74b80ba118e1feb85a59e438a8a742830e517
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2023-01-01 22:11:26 -0800

    Add all files

---
+++ src/main.c
@@ -0,0 +1,57 @@
+#include "termcolor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WORDS 5
+#define COLS 5
+
+void help() { printf("Usage: otpg [rows]\n\n"); }
+
+void panic() {
+    help();
+    exit(1);
+}
+
+void error(char *msg) {
+    cprint("ERROR: ", FG_RED);
+    printf(msg);
+    panic();
+}
+
+void warn(char *msg) {
+    cprint("WARN: ", FG_YELLOW);
+    printf(msg);
+}
+
+void generate(int rows) {
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            for (int k = 0; k < WORDS; ++k) {
+                int v = rand() % 26;
+                printf("%c ", (char)(v + 65));
+            }
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        error("Too few arguments.\n");
+    }
+
+    if (argc > 2) {
+        error("Too many arguments.\n");
+    }
+
+    int rows = atoi(argv[1]);
+
+    srand(time(NULL));
+
+    generate(rows);
+
+    return 0;
+}
