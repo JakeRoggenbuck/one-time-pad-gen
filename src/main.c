442977f10f48de735a354993ed6d58729abe4aa5
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2023-01-01 22:42:30 -0800

    Add main

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define WORDS 5
 #define COLS 5
@@ -24,12 +25,14 @@ void warn(char *msg) {
     printf(msg);
 }
 
+char tochar(int i) { return (char)(i + 65); }
+
 void generate(int rows) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < COLS; ++j) {
             for (int k = 0; k < WORDS; ++k) {
                 int v = rand() % 26;
-                printf("%c ", (char)(v + 65));
+                printf("%c ", tochar(v));
             }
             printf(" ");
         }
@@ -37,32 +40,55 @@ void generate(int rows) {
     }
 }
 
+void ref_pad() {
+    for (int i = 0; i < 26; ++i) {
+        printf("%c: ", tochar(i));
+        for (int j = 0; j < 26; ++j) {
+            printf("%c ", tochar(j));
+        }
+        printf("\n");
+
+        printf("   ");
+        for (int j = 0; j < 26; ++j) {
+            printf("%c ", tochar((j + i) % 26));
+        }
+        printf("\n");
+        printf("\n");
+    }
+}
+
 int get_seed() {
-	int val;
-	FILE *fp;
+    int val;
+    FILE *fp;
 
-	fp = fopen("/dev/urandom", "rb");
-	fread(&val, sizeof(int), 1, fp);
-	fclose(fp);
+    fp = fopen("/dev/urandom", "rb");
+    fread(&val, sizeof(int), 1, fp);
+    fclose(fp);
 
-	return val;
+    return val;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		error("Too few arguments.\n");
-	}
+    if (argc < 2) {
+        error("Too few arguments.\n");
+    }
 
-	if (argc > 2) {
-		error("Too many arguments.\n");
-	}
+    if (argc > 3) {
+        error("Too many arguments.\n");
+    }
 
-	int rows = atoi(argv[1]);
+    int rows = atoi(argv[1]);
 
-	int seed = get_seed();
-	srand(time(NULL) * seed);
+    if (argc == 3) {
+        if (strcmp(argv[2], "--ref") != -1) {
+            ref_pad();
+        }
+    }
 
-	generate(rows);
+    int seed = get_seed();
+    srand(time(NULL) * seed);
 
-	return 0;
+    generate(rows);
+
+    return 0;
 }
